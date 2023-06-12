12e3236b0c7a8b99a844f38200ff26004bb09a9d
SuperKeith333 <keithsalmon737@gmail.com>
2023-06-12 06:09:38 -0400

    j

---
+++ src/main/java/net/skvr/superkeiththemod/items/ModItems.java
@@ -0,0 +1,22 @@
+package net.skvr.superkeiththemod.items;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.skvr.superkeiththemod.SuperKeithTheMod;
+
+public class ModItems {
+    // rwa
+    public static final DeferredRegister<Item> ITEMS
+            = DeferredRegister.create(ForgeRegistries.ITEMS, SuperKeithTheMod.MOD_ID);
+
+    public static final RegistryObject<Item> BLANK_CIRCUIT_BOARD = ITEMS.register("blank_circuit_board",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
